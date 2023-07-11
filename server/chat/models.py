f3d632294267cb05f39c36f35281bfddecd35604
Ifiok Ambrose <61599279+Ambrose280@users.noreply.github.com>
2023-07-11 14:42:34 +0100

    Tweaked a few stuff

--- server/chat/models.py
+++ server/chat/models.py
@@ -2,15 +2,10 @@ from django.db import models
 from shortuuidfield import ShortUUIDField
 from user.models import User
 
-class ChatUser(models.Model):
-	username = models.CharField(max_length=256)
-	
-
-
 class ChatRoom(models.Model):
 	roomId = ShortUUIDField()
 	type = models.CharField(max_length=10, default='DM')
-	member = models.ManyToManyField(ChatUser)
+	member = models.ManyToManyField(User)
 	name = models.CharField(max_length=20, null=True, blank=True)
 
 	def __str__(self):
@@ -18,7 +13,7 @@ class ChatRoom(models.Model):
 
 class ChatMessage(models.Model):
 	chat = models.ForeignKey(ChatRoom, on_delete=models.SET_NULL, null=True)
-	user = models.ForeignKey(ChatUser, on_delete=models.SET_NULL, null=True)
+	user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
 	message = models.CharField(max_length=255)
 	timestamp = models.DateTimeField(auto_now_add=True)
 
